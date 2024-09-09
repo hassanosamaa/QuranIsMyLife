b0ae16d74b311aca6e670e7342415f454fdf95c5
hassanosamaa <almsryh877@gmail.com>
2024-09-09 16:48:48 +0300

    update_

--- src/components/PageCom/Header.js
+++ src/components/PageCom/Header.js
@@ -7,12 +7,13 @@ import sh_4 from "/public/sh_4.png";
 import sh_5 from "/public/sh_5.png";
 import sh_6 from "/public/sh_6.png";
 import AudioHome from "../GeneralCom/AudioHome";
-import fresh from "/public/refresh.svg"
+import fresh from "/public/refresh.svg";
+import { motion } from "framer-motion";
 
 export default function Header() {
-  const [isClient, setIsClient] = useState(false); 
-  const [refresh, setRefresh] = useState(1); 
-
+  const [isClient, setIsClient] = useState(false);
+  const [refresh, setRefresh] = useState(1);
+  const [animate, setAnimate] = useState(false);
 
   const imagesReciters = [sh_1, sh_2, sh_3, sh_4, sh_5, sh_6];
   const srcReciters = [
@@ -31,11 +32,10 @@ export default function Header() {
     " محمود علي البنا",
     " ناصر القطامي",
   ];
-  const handleRefresh=()=>{
+  const handleRefresh = () => {
     const randomNumber = Math.floor(Math.random() * 6);
-    setRefresh(randomNumber)
-
-  }
+    setRefresh(randomNumber);
+  };
 
   useEffect(() => {
     setIsClient(true);
@@ -46,8 +46,10 @@ export default function Header() {
   }
 
   return (
-    <div className="relative overflow-hidden flex justify-between items-center pt-5 pr-2 
-     pb-0 shadow-md mb-[18px] rounded-[20px] bg-gradient-to-r from-sky-950 to-sky-500 ">
+    <div
+      className="relative overflow-hidden flex justify-between items-center pt-5 pr-2 
+     pb-0 shadow-md mb-[18px] rounded-[20px] bg-gradient-to-r from-sky-950 to-sky-500 "
+    >
       <Image
         className="w-[170px] md:w-[480px] h-[170px] md:h-[350px]"
         src={imagesReciters[refresh]}
@@ -58,9 +60,22 @@ export default function Header() {
           حياتي مع القران
         </h2>
         <AudioHome src={srcReciters[refresh]} />
-        <h3 className="text-gray-900 font-[700] text-[12px] md:text-[18px] ">{nameReciters[refresh]}</h3>
+        <h3 className="text-gray-900 font-[700] text-[12px] md:text-[18px] ">
+          {nameReciters[refresh]}
+        </h3>
         <button className="absolute top-0 right-0 p-4" onClick={handleRefresh}>
-          <Image width={25} height={25} src={fresh} alt="fresh"/>
+          <motion.div
+            onClick={()=>{setAnimate(! animate)}}
+            initial={{ rotate: 0 }}
+            animate={{ rotate: animate? 360 : 0 }}
+            transition={{ duration: 1, ease: "easeInOut"}}
+          >
+            <Image
+              className="w-[10px] h-[10px] md:w-[20px] md:h-[20px] "
+              src={fresh}
+              alt="fresh"
+            />
+          </motion.div>
         </button>
       </div>
     </div>
